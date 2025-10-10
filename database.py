19dc1ced94d3a9ec53224ae11604ed6cf5b2ef93
sidneylcarneiro <sidneylcarneiro@yahoo.com>
2025-10-10 07:57:09 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Importa as configurações do nosso novo arquivo config.py
 from config import settings
 
-# A URL do banco de dados agora é lida a partir das configurações,
-# que por sua vez lêem do arquivo .env.
-# Nenhuma informação sensível fica mais no código.
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
